077b5171d617577efd23bb637d2fb2ea16868dee
Janessa Link <janessa@gmail.com>
2015-06-23 17:04:44 -0700

    [refactor] Code clean-up

--- chart.js
+++ chart.js
@@ -1,42 +1,24 @@
-$('#symbolForm').keydown(function() {
-  var key = e.which;
-  if (key == 13) {
-  // As ASCII code for ENTER key is "13"
-  $('#symbolForm').submit(); // Submit form code
-}
 
-
-
-
-});
-
-
+// On page load
   $(function(){
-
     var sym = $.url().param('symbol') || 'SLB';
     var dur = $.url().param('duration') || 3650;
-
     new Markit.InteractiveChartApi(sym, dur);
   });
 
-//Handles drop-down menu
-    function symbolLookUp(symbol){
-      var symbol = symbol.value;
-      console.log("Inside lookUp(): ", symbol);
+// Handles drop-down menu
+    function symbolLookUp(option){
+      var symbol = option.value;
       var sym = symbol || "SLB";
       var dur =  3650;
-
       new Markit.InteractiveChartApi(sym, dur);
 
    };
 
-//Handles type-in ticker form
+// Handles type-in ticker form
 $('form').submit(function(event){
       event.preventDefault();
       var symbol = $("input").val()
-      //var duration;
-
-      //console.log("Inside lookUp: ", symbol );
       var sym = symbol || "SLB";
       var dur =  3650;
 
